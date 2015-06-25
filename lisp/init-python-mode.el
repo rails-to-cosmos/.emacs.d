afb431933ccd9cf746794fa5dc1c422a60513b45
bezdnaskov <akatovda@gmail.com>
2015-06-26 01:18:50 +0300

    python breakpoints

--- lisp/init-python-mode.el
+++ lisp/init-python-mode.el
@@ -3,5 +3,23 @@
 		("SConscript\\'" . python-mode))
               auto-mode-alist))
 
+(defun python-add-breakpoint ()
+  "Add a break point"
+  (interactive)
+  ;; (newline-and-indent)
+  (insert "import pdb; pdb.set_trace()")
+  (highlight-lines-matching-regexp "^[ ]*import pdb; pdb.set_trace()"))
+
+;; (defun python-interactive ()
+;;   "Enter the interactive Python environment"
+;;   (interactive)
+;;   (progn
+;;     (insert "!import code; code.interact(local=vars())")
+;;     (move-end-of-line 1)
+;;     (comint-send-input)))
+
+;; (global-set-key (kbd "C-c i") 'python-interactive)
+
+(define-key python-mode-map (kbd "C-c C-b") 'python-add-breakpoint)
 
 (provide 'init-python-mode)
