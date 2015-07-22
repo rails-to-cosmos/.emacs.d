3a3c94a75e2f9c982b1e92a251f1fe43fd9d1be1
bezdnaskov <akatovda@gmail.com>
2015-07-22 22:13:26 +0300

    python mode from sacha lua

--- lisp/init-python-mode.el
+++ lisp/init-python-mode.el
@@ -10,6 +10,53 @@
   (insert "import pdb; pdb.set_trace()")
   (highlight-lines-matching-regexp "^[ ]*import pdb; pdb.set_trace()"))
 
+;; python-mode
+;; ============
+;; pre-requisites on ubuntu
+;; sudo pip install --upgrade pip
+;; sudo pip install jedi json-rpc --upgrade
+
+;; Deprecate these and try on a fresh machine new.
+;; sudo apt-get install -y python-dev python-setuptools python-pip python-virtualenv virtualenvwrapper
+;; sudo apt-get install -y python-flake8 pylint pep8 python-autopep8 python-jedi python-six
+;; Experimenting with python3, but not gotten it working yet
+;; (setq python-python-command "/home/sid/.virtualenvs/emacs/bin/python")
+;; (setq python-shell-interpreter "/usr/bin/python3")
+(add-hook 'python-mode-hook
+          (lambda ()
+            (setq indent-tabs-mode nil)
+            (setq tab-width 4)
+            (setq python-indent-offset 4)))
+
+;; anaconda
+(use-package anaconda-mode
+  :ensure t
+  :config
+  (add-hook 'python-mode-hook 'anaconda-mode)
+  (add-hook 'python-mode-hook 'eldoc-mode))
+(use-package company-anaconda
+  :ensure t
+  :config
+  (add-to-list 'company-backends 'company-anaconda))
+
+;; virtualenv
+(use-package virtualenvwrapper
+  :ensure t
+  :config
+  (venv-initialize-interactive-shells) ;; if you want interactive shell support
+  (venv-initialize-eshell) ;; if you want eshell support
+  (setq venv-location "~/.virtualenvs/"))
+;; (setq python-shell-virtualenv-path "~/.virtualenvs/default")
+
+;; pep8
+;; (use-package python-pep8
+;;  :ensure t)
+;; (use-package py-autopep8
+;;  :ensure t)
+;; (setq py-autopep8-options '("--ignore=E309,"))
+
+
+
 ;; (defun python-interactive ()
 ;;   "Enter the interactive Python environment"
 ;;   (interactive)
