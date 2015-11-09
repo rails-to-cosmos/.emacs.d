12602228c5f6805f6db8a7bbe3707b1bea4fea7f
Dmitry Akatov <akatovda@yandex.com>
2015-11-09 21:57:45 +0300

    minor bugfixes

--- init.el
+++ init.el
@@ -559,32 +559,6 @@
        mode-line-misc-info
        mode-line-end-spaces))))
 
-(use-package miniedit
-  :defer t
-  :ensure t
-  :commands minibuffer-edit
-  :init (miniedit-install))
-
-;; (defun set-frame-size-according-to-resolution ()
-;;   (interactive)
-;;   (if window-system
-;;       (progn
-;;         ;; use 120 char wide window for largeish displays
-;;         ;; and smaller 80 column windows for smaller displays
-;;         ;; pick whatever numbers make sense for you
-;;         (if (> (x-display-pixel-width) 1280)
-;;             (add-to-list 'default-frame-alist (cons 'width 160)))
-;;         ;; for the height, subtract a couple hundred pixels
-;;         ;; from the screen height (for panels, menubars and
-;;         ;; whatnot), then divide by the height of a char to
-;;         ;; get the height we want
-;;         (add-to-list 'default-frame-alist
-;;                      (cons 'height (/ (- (x-display-pixel-height) 80)
-;;                                       (frame-char-height)))))))
-
-;; (when window-system
-;;   (set-frame-size-according-to-resolution))
-
 (load-theme 'zerodark t)
 (require 'init-gui-frames)
 
