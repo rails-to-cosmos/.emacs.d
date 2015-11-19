9cf8771cdb28bd2d1e551e9ad95360c5906281bd
Rails to Cosmos <akatovda@yandex.com>
2015-11-19 17:09:02 +0300

    unvimish-fold

--- lisp/init-editing-utils.el
+++ lisp/init-editing-utils.el
@@ -407,11 +407,4 @@ point reaches the beginning or end of the buffer, stop there."
         (goto-char (marker-position (car (last mark-ring))))))))
 (global-set-key (kbd "C-c C-n C-n") 'unpop-to-mark-command)
 
-(use-package vimish-fold
-  :config (vimish-fold-global-mode t)
-  :bind (("C-c v f f" . vimish-fold)
-         ("C-c v f r" . vimish-fold-refold)
-         ("C-c v f u" . vimish-fold-unfold)
-         ("C-c v f d" . vimish-fold-delete)))
-
 (provide 'init-editing-utils)
