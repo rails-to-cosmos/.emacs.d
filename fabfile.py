8bae4afb25c436b5a655ad7866465a1e70d44fd8
Rails to Cosmos <akatovda@yandex.com>
2016-02-06 22:20:42 +0300

    Fabric automatic commit

--- fabfile.py
+++ fabfile.py
@@ -2,6 +2,6 @@ from fabric.api import local, settings
 
 
 def push():
-    with settings(prompts={'Stage this hunk [y,n,q,a,d,/,e,?]?': 'y'}):
+    with settings(prompts={'Stage this hunk [y,n,q,a,d,/,s,e,?]? ': 'y'}):
         local('git add -p && git commit -am\'Fabric automatic commit\'')
         local('git push')
