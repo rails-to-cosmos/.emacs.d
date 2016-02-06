2e9159390e6ca0fa5892e60a997057dc99688148
Rails to Cosmos <akatovda@yandex.com>
2016-02-06 22:17:57 +0300

    Fabric automatic commit

--- fabfile.py
+++ fabfile.py
@@ -1,5 +1,9 @@
 from fabric.api import local
+from ilogue.fexpect import expect, expecting, run
+
+prompts = [expect('Stage this hunk [y,n,q,a,d,/,e,?]?', 'y')]
 
 def push():
-    local('git add -p && git commit -am\'Fabric automatic commit\'')
-    local('git push')
+    with expecting(prompts):
+        local('git add -p && git commit -am\'Fabric automatic commit\'')
+        local('git push')
