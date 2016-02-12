0c290e097f9acedf146d7d3853989b7464016c96
Rails to Cosmos <akatovda@yandex.com>
2016-02-12 19:22:52 +0300

    fabfile bugfixes

--- fabfile.py
+++ fabfile.py
@@ -2,7 +2,7 @@ from fabric.api import local, settings
 
 
 def push(cm):
-    with settings():
+    with settings(warn_only=True):
         local('git commit -am\'{cm}\''.format(cm=cm))
         local('git pull origin master')
         local('git commit -am\'Merge commit\'')
