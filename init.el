034626c802d40b584c09fcd86768404f8b8177fc
Rails to Cosmos <akatovda@yandex.com>
2016-09-14 10:53:15 +0300

    Yas.

--- init.el
+++ init.el
@@ -440,6 +440,34 @@
                       ;; (use-package auto-complete-config)
                       ;; (ac-config-default)
 
+                      (use-package yasnippet
+                        :config (progn
+                                  ;; Completing point by some yasnippet key
+                                  (defun yas-ido-expand ()
+                                    "Lets you select (and expand) a yasnippet key"
+                                    (interactive)
+                                    (let ((original-point (point)))
+                                      (while (and
+                                              (not (= (point) (point-min) ))
+                                              (not
+                                               (string-match "[[:space:]\n]" (char-to-string (char-before)))))
+                                        (backward-word 1))
+                                      (let* ((init-word (point))
+                                             (word (buffer-substring init-word original-point))
+                                             (list (yas-active-keys)))
+                                        (goto-char original-point)
+                                        (let ((key (remove-if-not
+                                                    (lambda (s) (string-match (concat "^" word) s)) list)))
+                                          (if (= (length key) 1)
+                                              (setq key (pop key))
+                                            (setq key (ido-completing-read "key: " list nil nil word)))
+                                          (delete-char (- init-word original-point))
+                                          (insert key)
+                                          (yas-expand)))))
+
+                                  (yas-global-mode t))
+                        :ensure t)
+
                       (global-set-key (kbd "M-/") 'hippie-expand)
                       (setq hippie-expand-try-functions-list '(try-expand-dabbrev
                                                                try-expand-dabbrev-all-buffers
