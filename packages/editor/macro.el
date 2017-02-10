4367f8a42bb26ecd3e8505642fbef5b2b76a3180
Dmitry Akatov <akatovda@yandex-team.ru>
2017-02-10 16:22:35 +0300

    Increment number at point.

--- packages/editor/macro.el
+++ packages/editor/macro.el
@@ -1,8 +1,16 @@
+(defun increment-number-at-point ()
+  (interactive)
+  (skip-chars-backward "0-9")
+  (or (looking-at "[0-9]+")
+      (error "No number at point"))
+  (replace-match (number-to-string (1+ (string-to-number (match-string 0))))))
+(global-set-key (kbd "C-c +") 'increment-number-at-point)
+
 (defun save-macro (name)
   "save a macro. Take a name as argument
         and save the last defined macro under
         this name at the end of your .emacs"
-  (interactive "SName of the macro:") ; ask for the name of the macro
+  (interactive "SName of the macro:")  ; ask for the name of the macro
   (kmacro-name-last-macro name)        ; use this name for the macro
   (find-file user-init-file)   ; open ~/.emacs or other user init file
   (goto-char (point-max))      ; go to the end of the .emacs
