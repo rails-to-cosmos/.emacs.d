50166abfed3465e1eea65e8cc32bfe69a8b4265e
Dmitry Akatov <akatovda@yandex-team.ru>
2017-04-10 17:37:39 +0300

    Minor changes, add minor features.

--- packages/init-org.el
+++ packages/init-org.el
@@ -38,27 +38,6 @@
                             (org-todo 'done)
                           (org-todo 'todo)))))))
 
-            ;; Org-link: insert heading by default
-            (require 'mm-url) ; to include mm-url-decode-entities-string
-
-            (defun my-org-insert-link ()
-              "Insert org link where default description is set to html title."
-              (interactive)
-              (let* ((url (read-string "URL: "))
-                     (title (get-html-title-from-url url)))
-                (org-insert-link nil url title)))
-
-            (defun get-html-title-from-url (url)
-              "Return content in <title> tag."
-              (let (x1 x2 (download-buffer (url-retrieve-synchronously url)))
-                (save-excursion
-                  (set-buffer download-buffer)
-                  (beginning-of-buffer)
-                  (setq x1 (search-forward "<title>"))
-                  (search-forward "</title>")
-                  (setq x2 (search-backward "<"))
-                  (mm-url-decode-entities-string (buffer-substring-no-properties x1 x2)))))
-
             (defun ded/org-show-next-heading-tidily ()
               "Show next entry, keeping other entries closed."
               (interactive)
