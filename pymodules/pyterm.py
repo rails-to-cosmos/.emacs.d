c98dd71ff02d09adc3d5eb813309a78c32eba201
Dmitry Akatov <akatovda@yandex-team.ru>
2017-06-20 13:50:52 +0300

    Don't activate pyterm.

--- pymodules/pyterm.py
+++ pymodules/pyterm.py
@@ -3,7 +3,6 @@ cmds = [cmd.strip() for cmd in script.split('\n\t') if cmd.strip()]
 ascmds = '\n\t'.join(['do script "{cmd}" in front window'.format(cmd=cmd) for cmd in cmds])
 script = '''
 tell application "Terminal"
-  activate
   {ascmds}
 end tell
 '''.format(ascmds=ascmds)
