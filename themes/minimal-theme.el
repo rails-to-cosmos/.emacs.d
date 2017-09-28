9e37bc51cc30193a1b935fbc23c5019c09a35731
Rails to Cosmos <akatovda@yandex.com>
2017-09-28 21:30:32 +0300

    Emacs 24.3 fixes.

---
+++ themes/minimal-theme.el
@@ -0,0 +1,33 @@
+;;; minimal-theme.el --- A minimal theme based on default colors.
+;; This file is not part of GNU Emacs.
+;;; Commentary:
+;;; License:
+;; Use of this source code is governed by the 'Revised BSD License'
+;; which can be found in the LICENSE file.
+;;; Code:
+
+(deftheme minimal
+  "A minimal theme based on xterm-256 color set [dark version]")
+
+(custom-theme-set-faces
+ 'minimal
+ `(org-ellipsis                    ((t (:inherit hs-face))))
+
+ ;; Mode-line
+ `(mode-line ((t (:background "grey75" :box ,(list :line-width 4 :color "grey75")))))
+ `(mode-line-inactive ((t (:background "grey90" :foreground "grey20" :box ,(list :line-width 4 :color "grey90"))))))
+
+;; Set variables
+(custom-theme-set-variables
+ 'minimal)
+
+;;;###autoload
+(when load-file-name
+  (add-to-list 'custom-theme-load-path
+	       (file-name-as-directory (file-name-directory load-file-name))))
+
+(provide-theme 'minimal)
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
+;;; minimal-theme.el ends here
