e427a09dd87eb8dd47aede6c8ea8acbb61bff2cf
Dmitry Akatov <akatovda@yandex-team.ru>
2017-10-25 21:27:06 +0300

    Major cleanups.

--- lisp/init-visual-regexp.el
+++
@@ -1,8 +0,0 @@
-(use-package visual-regexp
-  :ensure t)
-
-(define-key global-map (kbd "C-c r") 'vr/replace)
-(define-key global-map (kbd "C-c q") 'vr/query-replace)
-(define-key global-map (kbd "C-c m") 'vr/mc-mark)
-
-(provide 'init-visual-regexp)
