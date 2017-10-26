c147dbac885d5881481c92cacc1a337f94ef5029
Dmitry Akatov <akatovda@yandex-team.ru>
2017-10-26 19:11:37 +0300

    Change pyterm.

--- pymodules/pyterm.py
+++ pymodules/pyterm.py
@@ -1,21 +1,20 @@
-#!/usr/bin/yb-python
-# [[file:~/Documents/Stuff/ya.org::run_terminal][run_terminal]]
-script="ls -la"
-script = script.split('\n\t')
-cmds = [cmd.strip() for cmd in script if cmd.strip()]
-ascmds = '\n\t'.join(['do script "{cmd}" in front window'.format(cmd=cmd) for cmd in cmds])
-script = '''
-tell application "Terminal"
-  activate
-  {ascmds}
-end tell
-'''.format(ascmds=ascmds)
-
 import subprocess
-proc = subprocess.Popen(
-    ['osascript', '-'],
-    stdin=subprocess.PIPE,
-    stdout=subprocess.PIPE
-)
-stdout_output = proc.communicate(script)[0]
-# run_terminal ends here
+
+def run_term(script):
+    script = script.split('\n\t')
+    cmds = '\n\t'.join(['do script "{cmd}" in front window'.format(cmd=cmd.strip()) for cmd in script if cmd.strip()])
+    script = '''
+    tell application "Terminal"
+      activate
+      {}
+    end tell
+    '''.format(cmds)
+    proc = subprocess.Popen(
+        ['osascript', '-'],
+        stdin=subprocess.PIPE,
+        stdout=subprocess.PIPE
+    )
+    return proc.communicate(script)[0]
+
+if __name__ == '__main__':
+    run_term('ls -la')
