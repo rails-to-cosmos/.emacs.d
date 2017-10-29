06cb1ecdc97cfec089f6e9de64deed9e51b9105b
Dmitry Akatov <akatovda@yandex.com>
2017-10-29 14:47:09 +0300

    Nexus 7.

--- themes/minimal-theme.el
+++ themes/minimal-theme.el
@@ -1,33 +1,34 @@
-;;; minimal-theme.el --- A minimal theme based on default colors.
-;; This file is not part of GNU Emacs.
-;;; Commentary:
-;;; License:
-;; Use of this source code is governed by the 'Revised BSD License'
-;; which can be found in the LICENSE file.
-;;; Code:
 
-(deftheme minimal
-  "A minimal theme based on xterm-256 color set [dark version]")
+;; ;;; minimal-theme.el --- A minimal theme based on default colors.
+;; ;; This file is not part of GNU Emacs.
+;; ;;; Commentary:
+;; ;;; License:
+;; ;; Use of this source code is governed by the 'Revised BSD License'
+;; ;; which can be found in the LICENSE file.
+;; ;;; Code:
 
-(custom-theme-set-faces
- 'minimal
- `(org-ellipsis                    ((t (:inherit hs-face))))
+;; (deftheme minimal
+;;   "A minimal theme based on xterm-256 color set [dark version]")
 
- ;; Mode-line
- `(mode-line ((t (:background "grey75" :box ,(list :line-width 4 :color "grey75")))))
- `(mode-line-inactive ((t (:background "grey90" :foreground "grey20" :box ,(list :line-width 4 :color "grey90"))))))
+;; (custom-theme-set-faces
+;;  'minimal
+;;  `(org-ellipsis                    ((t (:inherit hs-face))))
 
-;; Set variables
-(custom-theme-set-variables
- 'minimal)
+;;  ;; Mode-line
+;;  `(mode-line ((t (:background "grey75" :box ,(list :line-width 4 :color "grey75")))))
+;;  `(mode-line-inactive ((t (:background "grey90" :foreground "grey20" :box ,(list :line-width 4 :color "grey90"))))))
 
-;;;###autoload
-(when load-file-name
-  (add-to-list 'custom-theme-load-path
-	       (file-name-as-directory (file-name-directory load-file-name))))
+;; ;; Set variables
+;; (custom-theme-set-variables
+;;  'minimal)
 
-(provide-theme 'minimal)
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
-;;; minimal-theme.el ends here
+;; ;;;###autoload
+;; (when load-file-name
+;;   (add-to-list 'custom-theme-load-path
+;;                (file-name-as-directory (file-name-directory load-file-name))))
+
+;; (provide-theme 'minimal)
+;; ;; Local Variables:
+;; ;; no-byte-compile: t
+;; ;; End:
+;; ;;; minimal-theme.el ends here
