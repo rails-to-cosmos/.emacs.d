3dc2a38e1e5e5e44c2123ee680b9ac70d52ef8cb
Dmitry Akatov <akatovda@yandex-team.ru>
2017-11-06 21:37:30 +0300

    Minor improvements.

--- init.el
+++ init.el
@@ -7,7 +7,7 @@
 (dolist (package-archive
          '(("melpa" . "http://melpa.milkbox.net/packages/")
            ("melpa-stable" . "http://melpa-stable.milkbox.net/packages/")
-           ;; ("marmalade" . "http://marmalade-repo.org/packages/")
+           ("marmalade" . "http://marmalade-repo.org/packages/")
            ("elpy" . "https://jorgenschaefer.github.io/packages/")))
   (add-to-list 'package-archives package-archive))
 (package-initialize)
