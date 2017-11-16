cbb56fd691bd9eb5fcdfe71954db79cb9c2db4e2
Dmitry Akatov <akatovda@yandex-team.ru>
2017-11-16 14:51:49 +0300

    Cleanups.

--- packages/editor/folding.el
+++
@@ -1,7 +0,0 @@
-(use-package origami
-  :config (progn
-            (add-hook 'prog-mode-hook 'origami-mode)
-            (add-hook 'emacs-lisp-mode-hook 'origami-mode))
-  :ensure t)
-
-(provide 'folding)
