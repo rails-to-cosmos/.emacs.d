5e348e5f5433568dacf1bddf8064ded5e8960b3e
Rails to Cosmos <akatovda@yandex.com>
2019-08-31 19:59:40 +0300

    Remove cache from vc.

--- cache/org-glance-password-manager.el
+++
@@ -1,4 +0,0 @@
-`(((outline "/home/akatovda/sync/notes/inventory.org" "Purchase a keyboard" "Register at Amazon") (point . 142) (scope . "/home/akatovda/sync/notes/inventory.org"))
-((outline "/home/akatovda/sync/notes/understanding.org" "Information sources" "ACM oreilly learning (safari)") (point . 6206) (scope . "/home/akatovda/sync/notes/understanding.org"))
-((outline "/home/akatovda/sync/stuff/flocktory/flocktory.org" "Tasks" "Тупит sql-server" "microsoft sql server credentials") (point . 8623) (scope . "/home/akatovda/sync/stuff/flocktory/flocktory.org"))
-)
