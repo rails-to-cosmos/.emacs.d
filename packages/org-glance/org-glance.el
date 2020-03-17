763046dcb77e0ca41a09cbfa69254b33725af813
Rails to Cosmos <akatovda@yandex.com>
2020-03-17 21:26:05 +0300

    Fix glance arhive dirs.

--- packages/org-glance/org-glance.el
+++ packages/org-glance/org-glance.el
@@ -70,17 +70,19 @@
 (defun org-glance--get-file-archive-dirs (filename)
   (let* ((default-directory (file-name-directory filename))
          (glance-archive-dir-prop (format "#+%s:" org-glance-property--glance-archive-dir)))
-    (with-temp-buffer
-      (insert-file-contents-literally filename)
-      (goto-char (point-min))
-      (loop while (search-forward glance-archive-dir-prop nil t)
-            collect (-some->> (thing-at-point 'line)
-                      substring-no-properties
-                      (s-replace glance-archive-dir-prop "")
-                      s-trim
-                      file-truename)
-            into result
-            finally (return (or result (list default-directory)))))))
+    (if (file-exists-p filename)
+        (with-temp-buffer
+          (insert-file-contents-literally filename)
+          (goto-char (point-min))
+          (loop while (search-forward glance-archive-dir-prop nil t)
+                collect (-some->> (thing-at-point 'line)
+                          substring-no-properties
+                          (s-replace glance-archive-dir-prop "")
+                          s-trim
+                          file-truename)
+                into result
+                finally (return (or result (list default-directory)))))
+      (warn "File %s not found" filename))))
 
 (defun org-glance-scope--list-file-archives (filename)
   (let ((archive-dirs (org-glance--get-file-archive-dirs filename))
