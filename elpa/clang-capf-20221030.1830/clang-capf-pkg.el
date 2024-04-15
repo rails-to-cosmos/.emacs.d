01cc298161c4ef3e6a734ff101886ae28ec218f4
Dmitry Akatov <dmitry.akatov@protonmail.com>
2024-04-15 14:53:02 +0300

    upgrade

---
+++ elpa/clang-capf-20221030.1830/clang-capf-pkg.el
@@ -0,0 +1,14 @@
+(define-package "clang-capf" "20221030.1830" "Completion-at-point backend for c/c++ using clang"
+  '((emacs "24.4"))
+  :commit "5e4dfba90ce86bbc7ee61805edfca04fff93c291" :authors
+  '(("Philip K. <philipk [at] posteo [dot] net>"))
+  :maintainers
+  '(("Philip K. <philipk [at] posteo [dot] net>"))
+  :maintainer
+  '("Philip K. <philipk [at] posteo [dot] net>")
+  :keywords
+  '("c" "abbrev" "convenience")
+  :url "https://git.sr.ht/~pkal/clang-capf")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ elpa/highlight-doxygen-20200520.1713/highlight-doxygen-pkg.el
@@ -0,0 +1,12 @@
+(define-package "highlight-doxygen" "20200520.1713" "Highlight Doxygen comments" 'nil :commit "eec4874e2e89d4eb39091aad89a67dff8f8ec84c" :authors
+  '(("Anders Lindgren"))
+  :maintainers
+  '(("Anders Lindgren"))
+  :maintainer
+  '("Anders Lindgren")
+  :keywords
+  '("faces")
+  :url "https://github.com/Lindydancer/highlight-doxygen")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
