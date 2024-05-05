2679de60c2abf2d6de07c4892c8d74ef90ee9fff
Dmitry Akatov <dmitry.akatov@protonmail.com>
2024-05-05 21:07:35 +0300

    haskell

--- lisp/init-haskell.el
+++ lisp/init-haskell.el
@@ -1,123 +1,123 @@
 (setenv "LSP_USE_PLISTS" "true")
 (setq read-process-output-max (* 3 1024 1024))
 
-;; (use-package haskell-mode
-;;   :init (progn
-;;           (require 'haskell)
-;;           (require 'lsp)
-;;           (require 'flyspell)
-;;           (require 'lsp-ui)
-;;           (require 'which-key)
-
-;;           (defun my-haskell-mode-hook ()
-;;             (add-hook 'after-save-hook 'haskell-mode-generate-tags nil t)))
-;;   :defines interactive-haskell-mode-map
-;;   :config (progn
-;;             (cl-pushnew "[/\\\\]\\.devenv\\'" lsp-file-watch-ignored-directories)
-;;             (cl-pushnew "[/\\\\]\\.direnv\\'" lsp-file-watch-ignored-directories)
-
-;;             (setq lsp-use-plists t)
-
-;;             (with-eval-after-load 'lsp-mode
-;;               (add-to-list 'lsp-file-watch-ignored-directories "bazel-[^/\\\\]+\\'")
-;;               (add-to-list 'lsp-file-watch-ignored-directories "output[^/\\\\]+\\'")))
-;;   :hook ((interactive-haskell-mode . subword-mode)
-;;          (interactive-haskell-mode . company-mode)
-;;          (interactive-haskell-mode . company-quickhelp-mode)
-;;          (interactive-haskell-mode . smartparens-strict-mode)
-;;          (interactive-haskell-mode . lsp-deferred)
-;;          (haskell-mode . yas-minor-mode)
-;;          (interactive-haskell-mode . haskell-hoogle-start-server)
-;;          ;; (interactive-haskell-mode . my-haskell-mode-hook)
-;;          (haskell-literate-mode . lsp-deferred)
-;;          (lsp-after-initialize . lsp-ui-mode)
-;;          (lsp-after-initialize . lsp-enable-which-key-integration)
-;;          ;; (interactive-haskell-mode . flyspell-prog-mode)
-;;          ;; (interactive-haskell-mode . haskell-auto-insert-module-template)
-;;          )
-;;   :bind (:map interactive-haskell-mode-map
-;;               ("M-." . lsp-ui-peek-find-definitions)
-;;               ("M-?" . lsp-ui-peek-find-references)
-;;               ("C-c i" . lsp-ui-imenu)
-;;               :map lsp-ui-mode-map
-;;               ("C-c h" . lsp-ui-doc-show))
-
-;;   :custom
-;;   (gc-cons-threshold 100000000)
-;;   (lsp-haskell-formatting-provider "stylish-haskell")
-;;   ;; (lsp-haskell-plugin-tactics-global-on nil)
-;;   ;; (lsp-haskell-plugin-haddock-comments-global-on nil)
-;;   ;; (lsp-haskell-plugin-stan-global-on nil)
-;;   (lsp-ui-doc-show-with-mouse nil)
-;;   (lsp-ui-doc-max-height 50)
-;;   (lsp-ui-doc-header t)
-;;   (lsp-ui-doc-enable t)
-;;   (lsp-ui-sideline-show-hover t)
-;;   (lsp-ui-doc-use-childframe t)
-;;   (lsp-ui-doc-position 'top)
-;;   (lsp-ui-doc-include-signature t)
-;;   (lsp-ui-sideline-enable t)
-;;   (lsp-ui-flycheck-list-position 'top)
-;;   (lsp-ui-flycheck-live-reporting t)
-;;   (lsp-ui-peek-enable t)
-;;   (haskell-mode-hook '(interactive-haskell-mode
-;;                        capitalized-words-mode
-;;                        haskell-decl-scan-mode
-;;                        ;; haskell-indent-mode
-;;                        haskell-indentation-mode
-;;                        highlight-uses-mode
-;;                        ;; turn-on-haskell-unicode-input-method
-;;                        ))
-;;   (haskell-process-suggest-hoogle-imports nil)
-;;   (haskell-hoogle-server-command (lambda (port) (list "stack" "hoogle" "--" "server" "--local" "--port" (number-to-string port))))
-;;   (haskell-interactive-popup-errors nil)
-
-;;   :ensure t
-;;   :ensure smartparens
-;;   :ensure haskell-mode
-;;   :ensure company
-;;   :ensure company-ghci
-;;   :ensure company-cabal
-;;   :ensure company-quickhelp
-;;   :ensure treemacs
-;;   :ensure treemacs-all-the-icons
-;;   :ensure lsp-mode
-;;   :ensure lsp-ui
-;;   :ensure lsp-haskell
-;;   :ensure which-key)
-
 (use-package haskell-mode
+  :init (progn
+          (require 'haskell)
+          (require 'lsp)
+          (require 'flyspell)
+          (require 'lsp-ui)
+          (require 'which-key)
+
+          (defun my-haskell-mode-hook ()
+            (add-hook 'after-save-hook 'haskell-mode-generate-tags nil t)))
+  :defines interactive-haskell-mode-map
   :config (progn
-            (require 'xref)
-            (require 'eglot)
-            (require 'align)
+            (cl-pushnew "[/\\\\]\\.devenv\\'" lsp-file-watch-ignored-directories)
+            (cl-pushnew "[/\\\\]\\.direnv\\'" lsp-file-watch-ignored-directories)
 
-            (add-to-list 'eglot-server-programs '(haskell-mode . ("haskell-language-server-wrapper" "--lsp")))
+            (setq lsp-use-plists t)
 
-            (add-to-list 'align-rules-list '(haskell-types (regexp . "\\(\\s-+\\)\\(::\\|∷\\)\\s-+") (modes quote (haskell-mode literate-haskell-mode))))
-            (add-to-list 'align-rules-list '(haskell-assignment (regexp . "\\(\\s-+\\)=\\s-+") (modes quote (haskell-mode literate-haskell-mode))))
-            (add-to-list 'align-rules-list '(haskell-arrows (regexp . "\\(\\s-+\\)\\(->\\|→\\)\\s-+") (modes quote (haskell-mode literate-haskell-mode))))
-            (add-to-list 'align-rules-list '(haskell-left-arrows (regexp . "\\(\\s-+\\)\\(<-\\|←\\)\\s-+") (modes quote (haskell-mode literate-haskell-mode))))
-            )
-
-  :hook ((haskell-mode . eglot-ensure)
+            (with-eval-after-load 'lsp-mode
+              (add-to-list 'lsp-file-watch-ignored-directories "bazel-[^/\\\\]+\\'")
+              (add-to-list 'lsp-file-watch-ignored-directories "output[^/\\\\]+\\'")))
+  :hook ((interactive-haskell-mode . subword-mode)
+         (interactive-haskell-mode . company-mode)
+         (interactive-haskell-mode . company-quickhelp-mode)
+         (interactive-haskell-mode . smartparens-strict-mode)
+         (interactive-haskell-mode . lsp-deferred)
          (haskell-mode . yas-minor-mode)
-         (haskell-mode . company-mode)
-         (haskell-mode . smartparens-strict-mode)
-         ;; (haskell-mode . (lambda () (call-interactively #'xref-etags-mode)))
+         (interactive-haskell-mode . haskell-hoogle-start-server)
+         ;; (interactive-haskell-mode . my-haskell-mode-hook)
+         (haskell-literate-mode . lsp-deferred)
+         (lsp-after-initialize . lsp-ui-mode)
+         (lsp-after-initialize . lsp-enable-which-key-integration)
+         ;; (interactive-haskell-mode . flyspell-prog-mode)
+         ;; (interactive-haskell-mode . haskell-auto-insert-module-template)
          )
+  :bind (:map interactive-haskell-mode-map
+              ("M-." . lsp-ui-peek-find-definitions)
+              ("M-?" . lsp-ui-peek-find-references)
+              ("C-c i" . lsp-ui-imenu)
+              :map lsp-ui-mode-map
+              ("C-c h" . lsp-ui-doc-show))
 
   :custom
-  (eglot-autoshutdown t) ;; shutdown language server after closing last file
-  (eglot-confirm-server-initiated-edits nil) ;; allow edits without confirmation
-  (eglot-extend-to-xref t)
+  (gc-cons-threshold 100000000)
+  (lsp-haskell-formatting-provider "stylish-haskell")
+  ;; (lsp-haskell-plugin-tactics-global-on nil)
+  ;; (lsp-haskell-plugin-haddock-comments-global-on nil)
+  ;; (lsp-haskell-plugin-stan-global-on nil)
+  (lsp-ui-doc-show-with-mouse nil)
+  (lsp-ui-doc-max-height 50)
+  (lsp-ui-doc-header t)
+  (lsp-ui-doc-enable t)
+  (lsp-ui-sideline-show-hover t)
+  (lsp-ui-doc-use-childframe t)
+  (lsp-ui-doc-position 'top)
+  (lsp-ui-doc-include-signature t)
+  (lsp-ui-sideline-enable t)
+  (lsp-ui-flycheck-list-position 'top)
+  (lsp-ui-flycheck-live-reporting t)
+  (lsp-ui-peek-enable t)
+  (haskell-mode-hook '(interactive-haskell-mode
+                       capitalized-words-mode
+                       haskell-decl-scan-mode
+                       ;; haskell-indent-mode
+                       haskell-indentation-mode
+                       highlight-uses-mode
+                       ;; turn-on-haskell-unicode-input-method
+                       ))
+  (haskell-process-suggest-hoogle-imports nil)
+  (haskell-hoogle-server-command (lambda (port) (list "stack" "hoogle" "--" "server" "--local" "--port" (number-to-string port))))
+  (haskell-interactive-popup-errors nil)
 
   :ensure t
-  :ensure align
   :ensure smartparens
-  :ensure yasnippet
+  :ensure haskell-mode
   :ensure company
-  :ensure eglot)
+  :ensure company-ghci
+  :ensure company-cabal
+  :ensure company-quickhelp
+  :ensure treemacs
+  :ensure treemacs-all-the-icons
+  :ensure lsp-mode
+  :ensure lsp-ui
+  :ensure lsp-haskell
+  :ensure which-key)
+
+;; (use-package haskell-mode
+;;   :config (progn
+;;             (require 'xref)
+;;             (require 'eglot)
+;;             (require 'align)
+
+;;             (add-to-list 'eglot-server-programs '(haskell-mode . ("haskell-language-server-wrapper" "--lsp")))
+
+;;             (add-to-list 'align-rules-list '(haskell-types (regexp . "\\(\\s-+\\)\\(::\\|∷\\)\\s-+") (modes quote (haskell-mode literate-haskell-mode))))
+;;             (add-to-list 'align-rules-list '(haskell-assignment (regexp . "\\(\\s-+\\)=\\s-+") (modes quote (haskell-mode literate-haskell-mode))))
+;;             (add-to-list 'align-rules-list '(haskell-arrows (regexp . "\\(\\s-+\\)\\(->\\|→\\)\\s-+") (modes quote (haskell-mode literate-haskell-mode))))
+;;             (add-to-list 'align-rules-list '(haskell-left-arrows (regexp . "\\(\\s-+\\)\\(<-\\|←\\)\\s-+") (modes quote (haskell-mode literate-haskell-mode))))
+;;             )
+
+;;   :hook ((haskell-mode . eglot-ensure)
+;;          (haskell-mode . yas-minor-mode)
+;;          (haskell-mode . company-mode)
+;;          (haskell-mode . smartparens-strict-mode)
+;;          ;; (haskell-mode . (lambda () (call-interactively #'xref-etags-mode)))
+;;          )
+
+;;   :custom
+;;   (eglot-autoshutdown t) ;; shutdown language server after closing last file
+;;   (eglot-confirm-server-initiated-edits nil) ;; allow edits without confirmation
+;;   (eglot-extend-to-xref t)
+
+;;   :ensure t
+;;   :ensure align
+;;   :ensure smartparens
+;;   :ensure yasnippet
+;;   :ensure company
+;;   :ensure eglot)
 
 ;; (cl-defun citre-peek-jump-abort ()
 ;;   (interactive)
