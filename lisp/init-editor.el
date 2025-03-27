90f789e817b445706af2ab82c47efb20ee703dbe
Dmitry Akatov <dmitry.akatov@protonmail.com>
2025-03-27 22:08:00 +0100

    duplicate dwim

--- lisp/init-editor.el
+++ lisp/init-editor.el
@@ -115,41 +115,6 @@
 
 (advice-add #'kill-line :after #'my-kill-line)
 
-(defun duplicate-line (arg)
-  "Duplicate current line, leaving point in lower line."
-  (interactive "*p")
-
-  ;; save the point for undo
-  (setq buffer-undo-list (cons (point) buffer-undo-list))
-
-  ;; local variables for start and end of line
-  (let ((bol (save-excursion (beginning-of-line) (point)))
-        eol)
-    (save-excursion
-
-      ;; don't use forward-line for this, because you would have
-      ;; to check whether you are at the end of the buffer
-      (end-of-line)
-      (setq eol (point))
-
-      ;; store the line and disable the recording of undo information
-      (let ((line (buffer-substring bol eol))
-            (buffer-undo-list t)
-            (count arg))
-        ;; insert the line arg times
-        (while (> count 0)
-          (newline) ;; because there is no newline in 'line'
-          (insert line)
-          (setq count (1- count)))
-        )
-
-      ;; create the undo information
-      (setq buffer-undo-list (cons (cons eol (point)) buffer-undo-list)))
-    )                                   ; end-of-let
-
-  ;; put the point in the lowest line and return
-  (next-line arg))
-
 (defun increment-number-at-point ()
   (interactive)
   (save-excursion
@@ -167,7 +132,7 @@
 
 (global-set-key (kbd "C-+") #'increment-number-at-point)
 (global-set-key (kbd "C--") #'decrement-number-at-point)
-(global-set-key (kbd "C-c p") 'duplicate-line)
+(global-set-key (kbd "C-c p") 'duplicate-dwim)
 (define-key global-map (kbd "C-x k") #'kill-current-buffer)
 (define-key global-map (kbd "C-x y d c") #'desktop-clear)
 (define-key global-map (kbd "C-x y f f") #'toggle-frame-maximized)
