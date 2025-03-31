9211227ad809118ec747b4d7587dc9c9017114fa
Dmitry Akatov <dmitry.akatov@protonmail.com>
2025-03-31 11:40:49 +0200

    flymake etc

--- lisp/init-search.el
+++ lisp/init-search.el
@@ -13,40 +13,25 @@
   :bind (("C-c r" . rg-project))
   :ensure t)
 
-;; (use-package fzf
-;;   :load-path "~/sync/stuff/fzf.el/"
-;;   :config (progn
-;;             (require 'fzf)
-
-;;             (defun fzf-project (&optional with-preview)
-;;               "Starts an fzf session at the root of the current projectile project."
-;;               (interactive "P")
-;;               (pcase (my-project-root)
-;;                 (`(git ,dir) (fzf-git-files))
-;;                 (`(hg ,dir) (fzf-hg-files))
-;;                 (`(project ,dir) (let ((fzf/args (if with-preview (concat fzf/args " " fzf/args-for-preview) fzf/args))
-;;                                        (fzf--target-validator (fzf--use-validator (function fzf--validate-filename))))
-;;                                    (fzf--start () () #'fzf--action-find-file)))
-;;                 (otherwise 'nil))))
-;;   :bind (("C-x f" . fzf-project))
-;;   :ensure t)
-
 (defun my-fzf ()
   (interactive)
 
   (let ((fzf/executable "fd")
-        (fd-command (or (getenv "FZF_DEFAULT_COMMAND") "fd --type f --strip-cwd-prefix")))
+        (fd-command (or (getenv "FZF_DEFAULT_COMMAND") "fd --type f --strip-cwd-prefix"))
+        (default-directory (cadr (my-project-root))))
 
     (unless (executable-find fzf/executable t)
       (user-error "Can't find executable '%s'. Is it in your OS PATH?"
                   fzf/executable))
 
-    (find-file (completing-read "Find file: " (->> (save-window-excursion
-                                                     (eshell-command fd-command)
-                                                     (unwind-protect (with-current-buffer (get-buffer-create "*Eshell Command Output*")
-                                                                       (buffer-substring-no-properties (point-min) (point-max)))
-                                                       (bury-buffer "*Eshell Command Output*")))
-                                                   (s-split "\n"))))))
+    (->> (->> (save-window-excursion
+                (eshell-command fd-command)
+                (unwind-protect (with-current-buffer (get-buffer-create "*Eshell Command Output*")
+                                  (buffer-substring-no-properties (point-min) (point-max)))
+                  (bury-buffer "*Eshell Command Output*")))
+              (s-split "\n"))
+         (completing-read "Find file: ")
+         (find-file))))
 
 (global-set-key (kbd "C-x f") 'my-fzf)
 
