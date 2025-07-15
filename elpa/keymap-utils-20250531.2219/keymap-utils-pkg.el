121db15df48eeebb2b0e288e04b9e970381d40b8
Dmitry Akatov <dmitry.akatov@protonmail.com>
2025-07-15 21:28:25 +0200

    upgrade

---
+++ elpa/keymap-utils-20250531.2219/keymap-utils-pkg.el
@@ -0,0 +1,11 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "keymap-utils" "20250531.2219"
+  "Keymap utilities."
+  '((emacs  "26.1")
+    (compat "30.1"))
+  :url "https://github.com/tarsius/keymap-utils"
+  :commit "5acd65f35aa12303e3ea1f205c368e82f0fa89c3"
+  :revdesc "5acd65f35aa1"
+  :keywords '("convenience" "extensions")
+  :authors '(("Jonas Bernoulli" . "emacs.keymap-utils@jonas.bernoulli.dev"))
+  :maintainers '(("Jonas Bernoulli" . "emacs.keymap-utils@jonas.bernoulli.dev")))
