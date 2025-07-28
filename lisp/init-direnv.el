913c6ae88d407cd00586cb1debe2d6bc6968d78a
Dmitry Akatov <dmitry.akatov@protonmail.com>
2025-07-28 21:57:59 +0200

    disable direnv modes yet

--- lisp/init-direnv.el
+++ lisp/init-direnv.el
@@ -1,11 +1,11 @@
-(use-package mise
-  :config (progn
-            (global-mise-mode))
-  :ensure t)
+;; (use-package mise
+;;   :config (progn
+;;             (global-mise-mode))
+;;   :ensure t)
 
-(use-package envrc
-  :config (progn
-            (envrc-global-mode))
-  :ensure t)
+;; (use-package envrc
+;;   :config (progn
+;;             (envrc-global-mode))
+;;   :ensure t)
 
 (provide 'init-direnv)
