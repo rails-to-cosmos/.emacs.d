5710ffd244fc253449850abd44b788b9deaf77b5
Dmitry Akatov <dmitry.akatov@protonmail.com>
2025-08-07 12:48:11 +0200

    my direnv

--- lisp/init-direnv.el
+++ lisp/init-direnv.el
@@ -1,11 +1,14 @@
-;; (use-package mise
-;;   :config (progn
-;;             (global-mise-mode))
-;;   :ensure t)
+(use-package mise
+  :ensure t)
 
-;; (use-package envrc
-;;   :config (progn
-;;             (envrc-global-mode))
-;;   :ensure t)
+(use-package envrc
+  :ensure t)
+
+(cl-defun my-direnv ()
+  (interactive)
+  (or (-some->> (locate-dominating-file (buffer-file-name) "mise.toml")
+        (mise-mode))
+      (-some->> (locate-dominating-file (buffer-file-name) ".envrc")
+        (envrc-mode))))
 
 (provide 'init-direnv)
