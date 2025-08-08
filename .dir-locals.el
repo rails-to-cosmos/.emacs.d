6be3c24c7932c5e1798963d0892ba9fa74c3c411
Dmitry Akatov <dmitry.akatov@protonmail.com>
2025-08-08 11:56:10 +0200

    locals etc

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,5 +1,4 @@
 ;;; Directory Local Variables            -*- no-byte-compile: t -*-
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((nil . ((eval . (mise-mode))))
- (eshell-mode . ((foo . bar))))
+((nil . ((eval . (my-direnv)))))
--- lisp/init-direnv.el
+++ lisp/init-direnv.el
@@ -1,14 +1,26 @@
 (require 'files)
+(require 'strings)
 
 (use-package mise
   :ensure t)
+
+(cl-defun mise-trusted-p ()
+  (->> (with-output-to-string (mise--call standard-output "trust" "--show"))
+       (string-contains-p "untrusted")
+       (not)))
+
+(cl-defun mise-enable ()
+  (interactive)
+  (when (and (not (mise-trusted-p))
+             (yes-or-no-p "Mise config is untrusted. Trust it?"))
+    (message (with-output-to-string (mise--call standard-output "trust")))))
 
 (use-package envrc
   :ensure t)
 
 (cl-defun my-direnv ()
   (interactive)
-  (let ((direnv-file-mode-map '(("mise.toml" mise-mode)
+  (let ((direnv-file-mode-map '(("mise.toml" mise-enable)
                                 (".envrc" envrc-mode))))
     (cl-loop for (mode-file mode-hook) in direnv-file-mode-map
              when (locate-dominating-file default-directory mode-file)
