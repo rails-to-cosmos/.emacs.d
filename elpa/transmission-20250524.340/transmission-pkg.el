f566d1f423410e04cc4812656c418fbcd7f17037
Dmitry Akatov <dmitry.akatov@protonmail.com>
2025-10-04 22:23:44 +0200

    transmission client

---
+++ elpa/transmission-20250524.340/transmission-pkg.el
@@ -0,0 +1,11 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "transmission" "20250524.340"
+  "Interface to a Transmission session."
+  '((emacs     "24.4")
+    (let-alist "1.0.5"))
+  :url "https://github.com/holomorph/transmission"
+  :commit "ae36637fe63e530c7b8baa59bf566a99e40fbfe4"
+  :revdesc "ae36637fe63e"
+  :keywords '("comm" "tools")
+  :authors '(("Mark Oteiza" . "mvoteiza@udel.edu"))
+  :maintainers '(("Mark Oteiza" . "mvoteiza@udel.edu")))
